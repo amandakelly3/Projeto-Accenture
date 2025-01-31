a396b9f7593c86ec716b1b2723e3307fd9492a32
Amanda <89353180+amandakelly3@users.noreply.github.com>
2025-01-30 21:19:32 -0300

    Delete ProcessamentoAPI - Copia directory

--- ProcessamentoAPI - Copia/src/main/java/ProcessamentoAPI/repository/StatusPedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package ProcessamentoAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ProcessamentoAPI.entity.StatusPedido;
-
-public interface StatusPedidoRepository extends JpaRepository<StatusPedido, Integer> {
-}
